b1f7d1fe587ca25c1d4b8e79e58676c5cbe88d3d
Livia <livia.zampereti@hotmail.com>
2025-02-08 00:11:08 -0300

    Adição dos grippers

--- src/6dof_arm_rl/6dof_arm_rl/arm_control.py
+++ src/6dof_arm_rl/6dof_arm_rl/arm_control.py
@@ -10,10 +10,17 @@ class RobotCommander(Node):
 
     def send_command(self):
         msg = JointTrajectory()
-        msg.joint_names = ['base_arm1_joint', 'arm1_arm2_joint', 'arm2_arm3_joint', 'arm3_arm4_joint', 'arm4_arm5_joint', 'arm5_arm6_joint']
+        msg.joint_names = ['base_arm1_joint', 
+                           'arm1_arm2_joint', 
+                           'arm2_arm3_joint', 
+                           'arm3_arm4_joint', 
+                           'arm4_arm5_joint', 
+                           'arm5_arm6_joint',
+                           'arm6_gripper1_joint', 
+                           'arm6_gripper2_joint']
         
         point = JointTrajectoryPoint()
-        point.positions = [0.5, 0.5, -0.5, -0.5, 0.3, -0.2]
+        point.positions = [0.5, 0.5, -0.5, -0.5, 0.3, -0.2, 0.0, 0.0]
         point.time_from_start.sec = 2
         
         msg.points.append(point)
