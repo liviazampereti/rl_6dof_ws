617296969fe9a5b5984eb2f8f056334ff22dff5e
Livia <livia.zampereti@hotmail.com>
2025-02-16 22:20:10 -0300

    Revert "Merge pull request #2 from liviazampereti/feature/robot-description"
    This reverts commit 61715ed5378f951043dc30e66ede514f93bfa374, reversing
    changes made to dd4374d2a4829351ac0ce4308ec79332e86ced25.

--- build/6dof_arm_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/livia/rl_6dof_ws/src/6dof_arm_description
-# Build directory: /home/livia/rl_6dof_ws/build/6dof_arm_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
